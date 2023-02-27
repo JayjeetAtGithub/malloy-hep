9fdd22666ba451b164947429af1a6b983f4fb91b
Jayjeet Chakraborty <jc.github@rediffmail.com>
2023-02-26 23:41:26 -0800

    Add q3

---
+++ malloy_query/q3.sql
@@ -0,0 +1,28 @@
+WITH __stage0 AS (
+  SELECT 
+     ((floor((((CASE WHEN cross_join_sql.J."pt"<15 THEN 14.99 WHEN cross_join_sql.J."pt">60 THEN 60.01 ELSE cross_join_sql.J."pt" END))-0.15)*1.0/0.45))*0.45)+0.375 as "x",
+     COUNT( 1) as "y"
+  FROM (
+          SELECT
+              unnest(Jet) as J,
+              Jet,
+              Muon,
+              MET,
+              Electron,
+              Photon,
+              Tau,
+              HLT,
+              PV,
+              run,
+              event,
+              luminosityBlock
+          FROM read_parquet('/mnt/data/*.parquet')
+      ) as cross_join_sql
+  WHERE (abs(cross_join_sql.J."eta"))<1
+  GROUP BY 1
+  ORDER BY 1 ASC NULLS LAST
+)
+SELECT 
+   base."x" as "x",
+   base."y" as "y"
+FROM __stage0 as base

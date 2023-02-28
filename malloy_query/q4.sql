58af812bc8e30b8834fd6d48e74cbd75a609d0cf
Jayjeet Chakraborty <jc.github@rediffmail.com>
2023-02-28 11:53:32 -0800

    Add q4

---
+++ malloy_query/q4.sql
@@ -0,0 +1,40 @@
+WITH __stage0 AS (
+  SELECT
+     cross_join_sql."MET", 
+     COUNT(DISTINCT cross_join_sql."__distinct_key") as "c"
+  FROM (SELECT GEN_RANDOM_UUID() as __distinct_key, * FROM (
+          SELECT
+              unnest(Jet) as J,
+              Jet,
+              MET,
+          FROM read_parquet('dataset/hep1.parquet')
+      ) as x) as cross_join_sql
+  LEFT JOIN (select UNNEST(generate_series(1,
+          100000, --
+          -- (SELECT genres_length FROM movies limit 1),
+          1)) as __row_id) as Jet_0 ON  Jet_0.__row_id <= array_length(cross_join_sql."Jet")
+  WHERE cross_join_sql.J."pt">40
+  GROUP BY cross_join_sql."MET"
+  ORDER BY 1 desc NULLS LAST
+),
+
+__stage1 AS (
+SELECT 
+   base."MET" as "MET",
+FROM __stage0 as base
+WHERE base."c">1
+),
+
+__stage2 AS (
+  SELECT
+     ((floor((CASE WHEN hep.MET."pt"<0 THEN -1 WHEN hep.MET."pt">2000 THEN 2001 ELSE hep.MET."pt" END)*1.0/20))*20)+10 as "x",
+     COUNT( 1) as "y"
+  FROM __stage1 as hep
+  GROUP BY 1
+  ORDER BY 1 ASC NULLS LAST
+)
+
+SELECT
+   base."x" as "x",
+   base."y" as "y"
+FROM __stage2 as base

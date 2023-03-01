d87a1e09b25be11371b4a47af9f222664f4c34e4
Jayjeet Chakraborty <jc.github@rediffmail.com>
2023-03-01 13:10:09 -0800

    Add q4 working

--- malloy_query/q4.sql
+++ malloy_query/q4.sql
@@ -54,3 +54,28 @@
 --    base."y" as "y"
 -- FROM __stage0 as base
 -- this works, just a copy of Q1 with the unnest thing, but no perf improvement, obviously :(.
+WITH __stage0 AS (
+  SELECT 
+     hep."uid" as "uid",
+     ANY_VALUE(hep."MET") as "MET",
+  FROM (SELECT gen_random_uuid() uid, * FROM 'dataset/*.parquet') as hep
+  LEFT JOIN (select UNNEST(generate_series(1,
+          100000, --
+          -- (SELECT genres_length FROM movies limit 1),
+          1)) as __row_id) as Jet_0 ON  Jet_0.__row_id <= array_length(hep."Jet")
+  GROUP BY 1
+  HAVING (COUNT( CASE WHEN hep.Jet[Jet_0.__row_id]."pt">40 THEN 1 END)>1)
+  ORDER BY 1 asc NULLS LAST
+),
+__stage1 AS (
+  SELECT
+     ((floor((CASE WHEN hep.MET."pt"<0 THEN -1 WHEN hep.MET."pt">2000 THEN 2001 ELSE hep.MET."pt" END)*1.0/20))*20)+10 as "x",
+     COUNT( 1) as "y"
+  FROM __stage0 as hep
+  GROUP BY 1
+  ORDER BY 1 ASC NULLS LAST
+)
+SELECT
+   base."x" as "x",
+   base."y" as "y"
+FROM __stage1 as base

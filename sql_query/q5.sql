5342a3f2758942f760fdac8c3f38edb1e841b10d
Jayjeet Chakraborty <jc.github@rediffmail.com>
2023-02-28 21:26:40 -0800

    Update q5

--- sql_query/q5.sql
+++ sql_query/q5.sql
@@ -1,30 +1,18 @@
-
-WITH temp AS (
-  SELECT event, MET.pt, COUNT(*)
-  FROM {input_table}
-  CROSS JOIN UNNEST(Muon) WITH ORDINALITY AS _m1(m1, idx1)
-  CROSS JOIN UNNEST(Muon) WITH ORDINALITY AS _m2(m2, idx2)
-  WHERE
-    cardinality(Muon) > 1 AND
-    idx1 < idx2
-    AND m1.charge <> m2.charge AND
-    SQRT(2 * m1.pt * m2.pt * (COSH(m1.eta - m2.eta) - COS(m1.phi - m2.phi))) BETWEEN 60 AND 120
-  GROUP BY event, MET.pt
-  HAVING COUNT(*) > 0
-)
 SELECT
   FLOOR((
     CASE
-      WHEN pt < 0 THEN -1
-      WHEN pt > 2000 THEN 2001
-      ELSE pt
+      WHEN MET.pt < 0 THEN -1
+      WHEN MET.pt > 2000 THEN 2001
+      ELSE MET.pt
     END) / 20) * 20 + 10 AS x,
   COUNT(*) AS y
-FROM temp
-GROUP BY FLOOR((
-    CASE
-      WHEN pt < 0 THEN -1
-      WHEN pt > 2000 THEN 2001
-      ELSE pt
-    END) / 20) * 20 + 10
-ORDER BY x;
+FROM `{bigquery_dataset}.{input_table}`
+WHERE ARRAY_LENGTH(Muon) >= 2 AND
+  (SELECT COUNT(*) AS mass
+   FROM UNNEST(Muon) m1 WITH OFFSET i
+   CROSS JOIN UNNEST(Muon) m2 WITH OFFSET j
+   WHERE
+     m1.charge <> m2.charge AND i < j AND
+     SQRT(2*m1.pt*m2.pt*(COSH(m1.eta-m2.eta)-COS(m1.phi-m2.phi))) BETWEEN 60 AND 120) > 0
+GROUP BY x
+ORDER BY x

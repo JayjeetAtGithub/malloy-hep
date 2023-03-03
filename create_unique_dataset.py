79b26182a1cdd1984222a61c7016b2efbc385d5d
Jayjeet Chakraborty <jc.github@rediffmail.com>
2023-03-02 16:42:26 -0800

    Make script idempotent

--- create_unique_dataset.py
+++ create_unique_dataset.py
@@ -1,9 +1,13 @@
+import os
+
 import duckdb
 import pandas
 
 if __name__ == "__main__":
-
+    os.removedirs("/mnt/data/dataset_uuid", ignore_errors=True)
+    os.makedirs("/mnt/data/dataset_uuid", exist_ok=True)
     for i in range(1, 10001):
+
         sql = """
             SELECT generate_random_uuid() AS uuid, * FROM '/mnt/data/dataset/hep.{i}.parquet'
         """
